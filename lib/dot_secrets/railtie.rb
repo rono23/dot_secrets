9bab5ff93777c67c2e2432a70fabcab48351250c
rono23 <rono23@gmail.com>
2014-03-13 21:47:47 +0900

    Load dot_secrets before initialize

--- lib/dot_secrets/railtie.rb
+++ lib/dot_secrets/railtie.rb
@@ -1,7 +1,7 @@
 module DotSecrets
   class Railtie < ::Rails::Railtie
     initializer 'dot_secrets' do |app|
-      ActiveSupport.on_load(:after_initialize) do
+      ActiveSupport.on_load(:before_initialize) do
         ::ActiveSupport::OrderedOptions.send :include, DotSecrets::OrderedOptions
         ::Rails::Application.send :prepend, DotSecrets::Application
       end
